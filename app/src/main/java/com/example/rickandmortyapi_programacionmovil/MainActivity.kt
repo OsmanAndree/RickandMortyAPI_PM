04480c97dc5f46354f0239ca5fb3eddc94807956
osman barahona <osma_barahonas01@unicah.edu>
2024-12-09 16:32:40 -0600

    RickandMortyAPI_ProgramacionMovil

---
+++ app/src/main/java/com/example/rickandmortyapi_programacionmovil/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.rickandmortyapi_programacionmovil
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.rickandmortyapi_programacionmovil.ui.theme.RickandMortyAPI_ProgramacionMovilTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RickandMortyAPI_ProgramacionMovilTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RickandMortyAPI_ProgramacionMovilTheme {
+        Greeting("Android")
+    }
+}
